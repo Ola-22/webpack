9579d45a838a430a72af84344a781df57abcb749
Ola-22 <olanazly@gmail.com>
2022-03-29 13:50:03 +0300

    Add files

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports={
+    output:{
+        path: path.join(__dirname, "/dist"),
+        filename: "index.bundle.js",
+    },
+    devServer:{
+        static : {
+            directory : path.join(__dirname, "public/")
+          },
+          port: 3010,
+ 
+          hot: "only", 
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    loader:"babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ]
+            }
+        ]
+    },
+    plugins:[new MiniCssExtractPlugin()],
+}
